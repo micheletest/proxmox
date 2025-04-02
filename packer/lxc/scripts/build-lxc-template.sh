88f72702a521333fd56523b3c7ba849bdc9a3f15
Michele Martone <michele.martone@gmail.com>
2025-04-02 12:39:28 -0400

    add additional packages via shell

--- packer/lxc/scripts/build-lxc-template.sh
+++ packer/lxc/scripts/build-lxc-template.sh
@@ -9,7 +9,7 @@ TEMPLATE_PATH="/var/lib/vz/template/cache/${TEMPLATE_NAME}"
 
 echo "📦 Installing build tools"
 apt-get update
-apt-get install -y debootstrap tar xz-utils
+apt-get install -y debootstrap tar xz-utils systemd-container
 
 echo "Installing service tools"
 apt update && apt install -y \
@@ -29,6 +29,25 @@ debootstrap --arch="$ARCH" "$DISTRO" "$ROOTFS_DIR" http://deb.debian.org/debian
 echo "🧾 Configuring base system"
 echo "debian" > "$ROOTFS_DIR/etc/hostname"
 echo "127.0.0.1 localhost" > "$ROOTFS_DIR/etc/hosts"
+
+echo "🔧 Mounting for chroot"
+mount --bind /dev "$ROOTFS_DIR/dev"
+mount --bind /proc "$ROOTFS_DIR/proc"
+mount --bind /sys "$ROOTFS_DIR/sys"
+
+echo "🚪 Entering chroot to install openssh-server"
+chroot "$ROOTFS_DIR" /bin/bash -c "
+apt-get update
+apt-get install -y openssh-server sudo curl wget gnupg iproute2
+systemctl enable ssh
+sed -i 's/^#\\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+"
+
+echo "🔧 Unmounting chroot mounts"
+umount "$ROOTFS_DIR/dev"
+umount "$ROOTFS_DIR/proc"
+umount "$ROOTFS_DIR/sys"
 
 echo "🧼 Cleaning cache"
 rm -rf "$ROOTFS_DIR/var/cache/apt/archives/*.deb"
